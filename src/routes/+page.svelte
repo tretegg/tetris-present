f1d2528827587ca5af138699513e20d3e5a6ec05
Caiden Beaton <caiden_beaton@sd69qualicum.ca>
2025-01-13 09:53:43 -0800

    updated discriptions, added hover effect

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,6 +4,7 @@
 
 <!-- Game Discription -->
 <div>
-	<p class="pixel text-4xl text-center">Tetris</p>
-	<p class="pixel text-center">We made a Tetris clone with SvelteKit</p>
+	<p class="pixel text-3xl text-center mt-2 hover:text-4xl transition-all duration-200">Tetris</p>
+	<p class="pixel text-center">A Tetris clone made with <a href="https://svelte.dev/" class="text-blue-500 pixel">Sveltekit</a>, <a href="https://tailwindcss.com/" class="text-blue-500 pixel">TailwindCSS</a> and Typescript</p>
+	<p class="pixel text-center">Heavily inspired by <a href="https://tetr.io/" class="text-blue-500 pixel">tetr.io</a></p>
 </div>
