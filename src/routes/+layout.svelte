b9669ec0e063cdce17e273663c664898797c983f
Caiden Beaton <caiden_beaton@sd69qualicum.ca>
2025-01-14 10:27:23 -0800

    changed font

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,19 +4,19 @@
 	let title = "Tetris";
 	let links = [
 		{ name: "Home", href: "/" },
-		{ name: "About", href: "/about" },
-		{ name: "Contact", href: "/contact" },
+		{ name: "Play", href: "/play" },
+		{ name: "Github", href: "/github" },
 	];
 </script>
 
 <header class="header fixed top-0 left-0 w-full flex justify-between items-center
 		p-4 border-b-2 border-gray-200 backdrop-blur bg-white/70 z-50">
-	<div class="logo pixel">
+	<div class="logo inter-bold text-2xl">
 		<h1>{title}</h1>
 	</div>
 	<nav class="nav">
 		{#each links as link}
-			<a href={link.href} class="p-4 pixel">{link.name}</a>
+			<a href={link.href} class="p-4 inter-bold hover:text-xl transition-all duration-200">{link.name}</a>
 		{/each}
 	</nav>
 </header>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,7 +4,7 @@
 
 <!-- Game Discription -->
 <div>
-	<p class="pixel text-3xl text-center mt-2 hover:text-4xl transition-all duration-200">Tetris</p>
-	<p class="pixel text-center">A Tetris clone made with <a href="https://svelte.dev/" class="text-blue-500 pixel">Sveltekit</a>, <a href="https://tailwindcss.com/" class="text-blue-500 pixel">TailwindCSS</a> and Typescript</p>
-	<p class="pixel text-center">Heavily inspired by <a href="https://tetr.io/" class="text-blue-500 pixel">tetr.io</a></p>
+	<p class="inter-bold text-3xl text-center mt-2">Tetris</p>
+	<p class="inter text-center">A Tetris clone made with <a href="https://svelte.dev/" class="text-blue-500 inter-bold">Sveltekit</a>, <a href="https://tailwindcss.com/" class="text-blue-500 inter-bold">TailwindCSS</a> and Typescript</p>
+	<p class="inter text-center">Heavily inspired by <a href="https://tetr.io/" class="text-blue-500 inter-bold">tetr.io</a></p>
 </div>
